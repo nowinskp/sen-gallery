2d5a207c193ea2e448155cf3afb55652db2b378a
Piotr Nowiński <p.nowinski@creoignis.com>
2015-10-01 12:22:49 +0200

    Added loader & preliminary WPGalleryOutput filter

---
+++ load.php
@@ -0,0 +1,56 @@
+<?php
+
+# LOAD REQUIRED FILES
+# ------------------------------------------
+$files = [
+	// 'folder/filename',
+];
+
+foreach ($files as $file) {
+	require_once(__DIR__."/$file.php");
+}
+
+
+# SETUP CLASSES AUTOLOADER
+# ------------------------------------------
+spl_autoload_register(function($className){
+   $className = ltrim($className, '\\');
+   $folderName = __DIR__ . '/classes/';
+   if ($lastNsPos = strrpos($className, '\\')) {
+      $className = substr($className, $lastNsPos + 1);
+   }
+   $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+   if (file_exists($folderName.$fileName)) {
+      require $folderName.$fileName;
+      return true;
+   } else {
+      return false;
+   }
+});
+
+
+# SETUP FILTERS
+# ------------------------------------------
+add_filter( 'post_gallery', 'sen_galleries_getWPGalleryCode', 10, 2 );
+function sen_galleries_getWPGalleryCode( $output, $attr ) {
+	$code = new sen\galleries\WPGalleryOutput();
+	return $code->getOutput($output, $attr);
+}
+
+
+# ENQUEUE SCRIPTS
+# ------------------------------------------
+add_action( 'admin_enqueue_scripts', 'sen_loadGalleryScripts' );
+add_action( 'wp_enqueue_scripts', 'sen_loadGalleryScripts' );
+function sen_loadGalleryScripts(){
+
+	$currentDir = sen_get_current_dir_uri(__DIR__);
+
+	// wp_register_script(
+	// 	'sen-helpers-extend',
+	// 	$currentDir.'/js/extend.js',
+	// 	['jquery'], false, false
+	// );
+	// wp_enqueue_script( 'sen-helpers-extend' );
+
+}
