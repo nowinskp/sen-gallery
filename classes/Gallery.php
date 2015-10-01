f991b0651188dc62f3696f444c88f9e1c4e34163
Piotr Nowiński <p.nowinski@creoignis.com>
2015-10-01 12:31:32 +0200

    Extended Gallery class, several edits and fixes

--- classes/Gallery.php
+++ classes/Gallery.php
@@ -9,6 +9,7 @@ class Gallery {
 
 	// an array of images, in format of an associative array w/ keys:
 	// $images[#]['id']
+	// $images[#]['index'] <- !!! this set up automatically
 	// $images[#]['title']
 	// $images[#]['description']
 	// $images[#]['src']
@@ -16,7 +17,7 @@ class Gallery {
 	// $images[#]['image']['large']
 	// $images[#]['image']['full']
 	// $images[#]['link']
-	protected $images;
+	protected $images = [];
 
 	// an array of gallery setup options.
 	// supported options w/ their defaults:
@@ -34,14 +35,42 @@ class Gallery {
 	public function __construct($imagesArray, $options = []) {
 		$this->id = self::$instance;
 		self::$instance++;
-
 		$this->options = $options;
+		$this->loadImageArray($imagesArray);
+	}
 
+
+	/**
+	 * LOAD IMAGE ARRAY
+	 * Loads image array into the gallery object.
+	 * Sets up all important properties.
+	 * @param array $images - an array of gallery images w/ required keys:
+	 *                        $images[#]['id']
+	 *                        $images[#]['title']
+	 *                        $images[#]['description']
+	 *                        $images[#]['src']
+	 *                        $images[#]['image']['thumbnail']
+	 *                        $images[#]['image']['large']
+	 *                        $images[#]['image']['full']
+	 *                        $images[#]['link']
+	 */
+	protected function loadImageArray($imagesArray) {
 		$this->images = $imagesArray;
+		$this->setImageIndexes();
 		$this->imageCount = count($this->images);
-
 		$currentImage = $_GET['gal-'.$this->id.'-img'];
-		$this->currentImage = ( $currentImage ) ? intval($currentImage) : 0;
+		$this->currentImage = ( isset($this->images[$currentImage]) ) ? intval($currentImage) : 0;
+	}
+
+
+	/**
+	 * SET IMAGE INDEXES
+	 * sets $images array index to be available inside each $image array
+	 */
+	protected function setImageIndexes() {
+		foreach ($this->images as $index => &$image) {
+			$image['index'] = $index;
+		}
 	}
 
 
@@ -58,17 +87,68 @@ class Gallery {
 	}
 
 
-	public function getImageLink($image, $option = 'direct') {
-		# direct link
-		if ($option == 'direct' && $image['link']) {
-			return "<a class=\"sen-gal-btn sen-gal-image-link\" href=\"{$image['link']}\">Wyświetl obraz</a>";
+	/**
+	 * GET IMAGE URL
+	 * @param  array/int $image - either image array or image index
+	 * @param  string $option - available options:
+	 *                          -> direct - get direct link to image
+	 *                          -> gallery - get link to load image
+	 *                             in the gallery
+	 *                          -> next/prev - get link to load
+	 *                             next/prev image in the gallery
+	 * @return string - image url
+	 */
+	public function getImageUrl($image, $option = 'gallery') {
+		$key = "gal-{$this->id}-img";
+		switch ($option) {
+			case 'direct':
+				return ( isset($image['link']) ) ? $image['link'] : '';
+			case 'gallery':
+				return add_var_to_url($key, $image['index'], get_current_url());
+			case 'next':
+				if (isset($this->images[$image['index']+1])) {
+					return add_var_to_url($key, $image['index']+1, get_current_url());
+				}
+				return add_var_to_url($key, 0, get_current_url());
+			case 'prev':
+				if (isset($this->images[$image['index']-1])) {
+					return add_var_to_url($key, $image['index']-1, get_current_url());
+				}
+				return add_var_to_url($key, $this->imageCount-1, get_current_url());
 		}
+	}
 
-		# prev/next link
-		if (in_array($option, ['next', 'prev']) ) {
-			// ====================================
-			// TO DO NEXT ...
-			// ====================================
+
+	/**
+	 * GET IMAGE LINK TAG
+	 * Returns <A> tag w/ link to the image
+	 * @param  array/int $image - either image array or image index
+	 * @param  string $option - available options:
+	 *                          -> direct - get direct link to image
+	 *                          -> gallery - get link to load image
+	 *                             in the gallery
+	 *                          -> next/prev <-></-> get link to load
+	 *                             next/prev image in the gallery
+	 * @return string - image url
+	 */
+	public function getImageLinkTag($image, $option = 'gallery') {
+		$link = $this->getImageUrl($image, $option);
+		if (!$link) {
+			return '';
+		}
+		switch ($option) {
+			case 'gallery':
+				$lbl = $label ? $label : 'Wyświetl w galerii';
+				return "<a class=\"sen-gal-btn sen-gal-image-link-gallery\" href=\"$link\">$lbl</a>";
+			case 'direct':
+				$lbl = $label ? $label : 'Wyświetl obraz';
+				return "<a class=\"sen-gal-btn sen-gal-image-link-direct\" href=\"$link\">$lbl</a>";
+			case 'prev':
+				$lbl = $label ? $label : 'Poprzedni';
+				return "<a class=\"sen-gal-btn sen-gal-image-link-prev\" href=\"$link\">$lbl</a>";
+			case 'next':
+				$lbl = $label ? $label : 'Następny';
+				return "<a class=\"sen-gal-btn sen-gal-image-link-next\" href=\"$link\">$lbl</a>";
 		}
 	}
 
@@ -104,9 +184,9 @@ class Gallery {
 		$output = "
 			<div class=\"sen-gallery-current-image-frame\">
 				<div class=\"sen-gallery-current-image-frame-buttons\">
-					{$this->getImageLink($img, 'previous')}
-					{$this->getImageLink($img, 'direct')}
-					{$this->getImageLink($img, 'next')}
+					{$this->getImageLinkTag($img, 'prev')}
+					{$this->getImageLinkTag($img, 'direct')}
+					{$this->getImageLinkTag($img, 'next')}
 				</div>
 				{$this->renderImage($img, 'full')}
 			</div>
@@ -124,7 +204,9 @@ class Gallery {
 		}
 		$output = '<div class="gallery-thumbnails">';
 		foreach ($this->images as $image) {
+			$output .= "<a href=\"{$this->getImageUrl($image, 'gallery')}\">";
 			$output .= $this->renderImage($image, 'thumbnail');
+			$output .= "</a>";
 		}
 		$output .= '</div>';
 		return $output;
